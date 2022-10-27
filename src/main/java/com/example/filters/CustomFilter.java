fbd2ec9125711cca9134e9d246fbc4a19a7ed07a
buciuman.liviu@yahoo.com <Mnopa_2@19>
2022-10-27 20:25:16 +0300

    filtru

--- src/main/java/com/example/filters/CustomFilter.java
+++ src/main/java/com/example/filters/CustomFilter.java
@@ -1,6 +1,15 @@
 package com.example.filters;
 
+import com.example.services.DetailsService;
+import com.example.services.UserService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -10,8 +19,40 @@ import java.io.IOException;
 
 public class CustomFilter extends OncePerRequestFilter {
 
+
+    @Autowired
+    private UserService userServiceUtil;
+
+    @Autowired
+    private DetailsService detailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        final String requestTokenHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String userService = null;
+
+        if(requestTokenHeader != null){
+            userService = requestTokenHeader;
+            try {
+                username = userServiceUtil.getSubject(userService);
+            }catch(IllegalArgumentException e){
+                System.out.println("Unable to get token");
+            }catch (ExpiredJwtException e) {
+                System.out.println("Token has expired");
+            }
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+            UserDetails userDetails = this.detailsService.loadUserByUsername(username);
+            if(userServiceUtil.isValidToken(userService,username)){
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
---
+++ src/main/java/com/example/services/DetailsService.java
@@ -0,0 +1,21 @@
+package com.example.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class DetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if ("jailhouse".equals(username)) {
+            return new User("jailhouse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
