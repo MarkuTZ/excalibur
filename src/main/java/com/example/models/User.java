add22e1a105783bb8a26d09977f821ffb77d6032
Cristina Breaz <breaz.cr.cristina@utcluj.didatec.ro>
2022-12-05 20:44:43 +0200

    create models

--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long ID;
+    private long Id;
     @Column
     private String username;
     @Column
