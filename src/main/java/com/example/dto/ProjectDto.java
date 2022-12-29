a8d94daa32a0f76bd87dd0f766c082b313040378
Cristina Breaz <breaz.cr.cristina@utcluj.didatec.ro>
2022-12-29 21:42:16 +0200

    Created DTO class

---
+++ src/main/java/com/example/dto/ProjectDto.java
@@ -0,0 +1,19 @@
+package com.example.dto;
+
+import com.example.models.User;
+import com.example.models.enums.Status;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class ProjectDto {
+
+    private long id;
+    private String name;
+    private String description;
+    private Date createDate;
+    private Date deadline;
+    private User owner;
+    private Status projectStatus;
+
+}
