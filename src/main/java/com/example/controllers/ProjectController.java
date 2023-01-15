6719d37b17fdf4501e352e616c6db0b4a4a7e709
buciuman.liviu@yahoo.com <Mnopa_2@19>
2023-01-15 13:59:13 +0200

    createTask

--- src/main/java/com/example/controllers/ProjectController.java
+++ src/main/java/com/example/controllers/ProjectController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/projects")
-public class ProjectContoller {
+public class ProjectController {
 
 	@Autowired
 	private final ProjectService projectService;
