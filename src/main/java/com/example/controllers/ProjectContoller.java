e4549a2764461ed9c2c38b841b3f7bcce23a8d7e
buciuman.liviu@yahoo.com <Mnopa_2@19>
2023-01-15 13:53:14 +0200

    createTask

--- src/main/java/com/example/controllers/ProjectContoller.java
+++ src/main/java/com/example/controllers/ProjectContoller.java
@@ -26,6 +26,7 @@ public class ProjectContoller {
 
 	@PostMapping(value = { "/{projectID}/tasks" })
 	public Task createTask(@RequestBody Task task, @PathVariable("projectID") long projectID) {
+
 		return projectService.saveTaskInDb(task,projectID);
 	}
 
